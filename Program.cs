96b9a64d9e57a1f4494abe783bfbe8cb67eb75c0
GalGer06 <galambos.gergo06@gmail.com>
2026-02-17 13:52:26 +0100

    Program futtat

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            PizzaKemce pizzaKemce = new PizzaKemce(10);
+
+            Sef sef = new Sef(pizzaKemce);
+            Futar futar = new Futar(pizzaKemce);
+
+            Thread sefThread = new Thread(sef.Sutes);
+            Thread futarThread = new Thread(futar.Szallit);
+
+            sefThread.Start();
+            futarThread.Start();
         }
     }
 }
