3050fa14f8a9458f736c9e91d12056f796661a2c
GalGer06 <galambos.gergo06@gmail.com>
2026-02-17 13:20:42 +0100

    SzinkronPuffer

--- SzinkronPuffer.cs
+++ SzinkronPuffer.cs
@@ -8,6 +8,41 @@ namespace PizzaKemence
 {
     internal class SzinkronPuffer<T>
     {
-
+            private Queue<T> sor;
+            private int meret;
+            private object zar = new object();
+    
+            public SzinkronPuffer(int capacity)
+            {
+                this.meret = capacity;
+                this.sor = new Queue<T>();
+            }
+    
+            public void Add(T item)
+            {
+                lock (zar)
+                {
+                    while (sor.Count >= meret)
+                    {
+                        Monitor.Wait(zar);
+                    }
+                    sor.Enqueue(item);
+                    Monitor.PulseAll(zar);
+                }
+            }
+    
+            public T Remove()
+            {
+                lock (zar)
+                {
+                    while (sor.Count == 0)
+                    {
+                        Monitor.Wait(zar);
+                    }
+                    T item = sor.Dequeue();
+                    Monitor.PulseAll(zar);
+                    return item;
+                }
+        }
     }
 }
